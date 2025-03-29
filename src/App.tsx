21b879ac19db5f3bb33fe54d1b9fdc5003a02b17
mi <musailliasu7@gmail.com>
2025-03-29 19:56:54 +0100

    removed second chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
 import ChartComponent1 from "./component/ChartComponent1";
-import ChartComponent2 from "./component/ChartComponent2";
 
-const App = () => {
-
-    return (
-        <>
-            <ChartComponent1 />
-            <hr style={{margin: "1rem 0"}} />
-            <ChartComponent2 />
-        </>
-    )
-}
+const App = () => <ChartComponent1 />;
 
 export default App;
